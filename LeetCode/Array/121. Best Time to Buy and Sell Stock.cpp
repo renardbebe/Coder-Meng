d1bfc4cb0aa6c22eae4229171428cc82eec76427
renardbebe <352600801@qq.com>
2020-06-21 22:40:58 +0800

    20200621

---
+++ LeetCode/Array/121. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxP = 0, minn = 0xffffff;
+        for (int i = 0; i < prices.size(); i++) {
+            minn = min(minn, prices[i]);
+            maxP = max(maxP, (prices[i] -  minn));
+        }
+        return maxP;
+    }
+};
---
+++ LeetCode/Array/122. Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minn = 0xffffff;
+        int maxSum = 0, maxP = 0;
+        for (int i = 0; i < prices.size(); i++) {
+            minn = min(minn, prices[i]);
+            maxP = max(maxP, (prices[i] - minn));
+            if (i != prices.size() - 1 && prices[i] > prices[i+1]) {
+                minn = 0xffffff;
+                maxSum += maxP;
+                maxP = 0;
+            }
+            if (i == prices.size() - 1) {
+                maxSum += maxP;
+            }
+            
+            // Or, simplest way is to divide total profit into daily sum: 
+            // maxSum += max(prices[i] - prices[i - 1], 0);    
+        }
+        return maxSum;
+    }
+};
